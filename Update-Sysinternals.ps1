c13e87e36734444aaf7903e2618b1415ce8f2a41
Brandon Adams <Brandon Adams>
2016-09-16 09:12:53 -0400

    Moved local path to parameter. Added gitignore

--- Update-Sysinternals.ps1
+++ Update-Sysinternals.ps1
@@ -1,8 +1,16 @@
 [CmdletBinding()]
-param()
+param
+(
+    # Path to the local copy of the Sysinternal tools
+    [Parameter(Mandatory = $true)]
+    [string]
+    $LocalPath
+)
 
-## Local path for Sysinternals tools
-$toolPath = ""
+if (!Test-Path -Path $LocalPath -PathType Container)
+{
+    throw "LocalPath must be a valid directory!"
+}
 
 ## Sysinternals SMB share
 $updateSource = "\\live.sysinternals.com\Tools"
@@ -12,19 +20,20 @@ Write-Verbose "Connecting to $updateSource"
 net use $updateSource | Out-Null
 
 ## loop over our current toolset
-Get-ChildItem -Path $toolPath | ForEach-Object {
+Get-ChildItem -Path $LocalPath | ForEach-Object {
     $currentTool = $_
 
     Write-Verbose "Checking: $($currentTool.Name)"
     
     $updatedTool = Get-Item (Join-Path -Path $updateSource -ChildPath $currentTool.Name)
     
+    ## compare last modified time in UTC
     if ($updatedTool.LastWriteTimeUtc -gt $currentTool.LastWriteTimeUtc)
     {
         Write-Host " Updating $($currentTool.BaseName)"
-        Write-Verbose "  >> Out of Date"
+        Write-Verbose "  >> Begin copy process"
         Copy-Item -Path $updatedTool.FullName -Destination $currentTool.FullName -Force | Out-Null
-        Write-Verbose "  >> Update complete"
+        Write-Verbose "  >> End copy process"
     }
 }
 
