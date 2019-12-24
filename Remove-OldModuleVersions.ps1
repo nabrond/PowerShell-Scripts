d5683ceeb53c80600066e4ad678774f896cd24ae
Brandon <bmadams@gmail.com>
2019-12-24 10:02:32 -0500

    Add script to clean old module versions.

---
+++ Remove-OldModuleVersions.ps1
@@ -0,0 +1,16 @@
+﻿[CmdletBinding()]
+param()
+
+Write-Verbose 'Checking installed modules...'
+
+Get-InstalledModule | ForEach-Object { 
+    $Latest = $_
+
+    Write-Verbose "  > $($_.Name)"
+
+    Get-InstalledModule $Latest.Name -AllVersions | 
+        Where-Object { $_.Version -ne $Latest.Version } | 
+        Uninstall-Module -Force -Verbose:$VerbosePreference
+}
+
+Write-Verbose 'Process complete!'
